c8e090e7ed5d11534528065c0c30922187b27e5a
Emigdio Torres <emigdio821@gmail.com>
2026-09-10 17:16:23 -0600

    chore: cleanup astro scaffolding

--- astro.config.ts
+++ astro.config.ts
@@ -1,10 +1,12 @@
-// @ts-check
 import { defineConfig } from 'astro/config'
 import tailwindcss from '@tailwindcss/vite'
 
 // https://astro.build/config
 export default defineConfig({
   vite: {
+    resolve: {
+      tsconfigPaths: true,
+    },
     plugins: [tailwindcss()],
   },
 })

81377dbc5be0c881cfbca82a7d772373f18e98e3
Emigdio Torres <emigdio821@gmail.com>
2026-09-10 20:44:47 -0600

    fix: eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,11 +1,45 @@
-import js from '@eslint/js'
-import eslintPluginAstro from 'eslint-plugin-astro'
-import { defineConfig } from 'eslint/config'
+// @ts-check
 
-export default defineConfig([
+import js from '@eslint/js'
+import * as astroParser from 'astro-eslint-parser'
+import eslintPluginAstro from 'eslint-plugin-astro'
+import tseslint from 'typescript-eslint'
+
+/** @type { import("eslint").Linter.Config[] } */
+
+export default [
+  {
+    ignores: ['dist', '.astro'],
+  },
   js.configs.recommended,
+  ...tseslint.configs.strictTypeChecked,
   ...eslintPluginAstro.configs.recommended,
   {
-    rules: {},
+    files: ['**/*.astro'],
+    languageOptions: {
+      parser: astroParser,
+      parserOptions: {
+        project: true,
+        parser: tseslint.parser,
+        extraFileExtensions: ['.astro'],
+      },
+    },
   },
-])
+  {
+    rules: {
+      'object-shorthand': 'error',
+      '@typescript-eslint/no-misused-promises': 'error',
+      '@typescript-eslint/no-floating-promises': 'error',
+      '@typescript-eslint/consistent-type-imports': 'error',
+      '@typescript-eslint/consistent-type-definitions': ['error', 'interface'],
+    },
+  },
+  {
+    languageOptions: {
+      parserOptions: {
+        project: true,
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+  },
+]
